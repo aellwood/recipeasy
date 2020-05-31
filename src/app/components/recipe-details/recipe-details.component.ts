e6f4ccd37bcab6be9422097e0a7438fb0f4f8e83
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2020-05-31 19:56:59 +0100

    added delete functionality

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Recipe } from "src/app/models/recipe";
+import { ApiService } from "src/app/services/api/api.service";
 
 @Component({
   selector: "app-recipe-details",
@@ -9,7 +10,15 @@ import { Recipe } from "src/app/models/recipe";
 export class RecipeDetailsComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {}
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {}
+
+  async deleteRecipe() {
+    //this.loading = true;
+    this.api.deleteRecipe(this.recipe.recipeId).subscribe((res) => {
+      //this.loading = false;
+      //this.recipeAdded.emit(res);
+    });
+  }
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,27 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
 
   ping$(): Observable<any> {
     return this.http.get(
-      'https://recipeasy-api.azurewebsites.net/api/values/testGet'
+      "https://recipeasy-api.azurewebsites.net/api/values/testGet"
     );
   }
 
   getRecipes(): Observable<any> {
-    return this.http.get('https://recipeasy-api.azurewebsites.net/api/recipes');
+    return this.http.get("https://recipeasy-api.azurewebsites.net/api/recipes");
   }
 
   postRecipe(body: any): Observable<any> {
     return this.http.post(
-      'https://recipeasy-api.azurewebsites.net/api/recipes',
+      "https://recipeasy-api.azurewebsites.net/api/recipes",
       body
+    );
+  }
+
+  deleteRecipe(id: string): Observable<any> {
+    return this.http.delete(
+      `https://recipeasy-api.azurewebsites.net/api/recipes/${id}`
     );
   }
 }
