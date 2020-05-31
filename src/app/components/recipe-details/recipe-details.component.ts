6dfff5d2b18e571bb0b399a87f5f6b5227508106
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2020-05-31 12:04:51 +0100

    styling changes

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  selector: "app-recipe-details",
+  templateUrl: "./recipe-details.component.html",
+  styleUrls: ["./recipe-details.component.scss"],
 })
 export class RecipeDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/recipe-name/recipe-name.component.ts
+++ src/app/components/recipe-name/recipe-name.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-name',
-  templateUrl: './recipe-name.component.html',
-  styleUrls: ['./recipe-name.component.css']
+  selector: "app-recipe-name",
+  templateUrl: "./recipe-name.component.html",
+  styleUrls: ["./recipe-name.component.scss"],
 })
 export class RecipeNameComponent implements OnInit {
+  @Input() recipeName: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
